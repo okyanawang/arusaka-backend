e7e24ea31486dd507bf19fa3733cbe1a2b274a6d
okyanawang <okyanawang@gmail.com>
2023-10-14 21:23:49 +0700

    Update course seeder

--- database/seeders/CoursesTableSeeder.php
+++ database/seeders/CoursesTableSeeder.php
@@ -21,72 +21,16 @@ class CoursesTableSeeder extends Seeder
         DB::table('courses')->insert(array (
             0 =>
             array (
-                'name' => 'Pengenalan Alat Musik Angklung toel',
-                'slug' => 'pengenalan-alat-musik-angklung-toel',
-                'description' => 'Belajar mengenai sejarah dan teknik dasar bermain alat musik Angklung toel.',
-                'status' => 'ACTIVE',
-                'image' => 'https://img.okezone.com/content/2012/12/08/408/729284/J7WcThtona.jpg',
-                'price' => 35000,
-                'categories_id' => 1,
-                'partners_id' => 1,
-            ),
-            1 =>
-            array (
-                'name' => 'Pengenalan Alat Musik Angklung Baduy',
-                'slug' => 'pengenalan-alat-musik-angklung-baduy',
-                'description' => 'Belajar mengenai sejarah dan teknik dasar bermain alat musik Angklung Baduy, alat musik khas daerah Baduy.',
-                'status' => 'ACTIVE',
-                'image' => 'https://sejarahlengkap.com/wp-content/uploads/2017/04/angklung.jpg',
-                'price' => 30000,
-                'categories_id' => 1,
-                'partners_id' => 1,
-            ),
-
-            2 =>
-            array (
-                'name' => 'Pengenalan Alat Musik Angklung reog',
-                'slug' => 'pengenalan-alat-musik-angklung-reog',
-                'description' => 'Belajar mengenai sejarah dan teknik dasar bermain alat musik Angklung reog, alat musik khas daerah reog.',
-                'status' => 'ACTIVE',
-                'image' => ' http://wadaya.rey1024.com/api/uploads/16522701321536283835.jpg',
-                'price' => 90000,
-                'categories_id' => 1,
-                'partners_id' => 1,
-            ),
-            3 =>
-            array (
-                'name' => 'Tutorial Angklung Bali',
-                'slug' => 'tutorial-angklung-bali',
-                'description' => 'Belajar memainkan angklung, alat musik khas Bali yang unik dan menarik.',
-                'status' => 'ACTIVE',
-                'image' => 'https://res.cloudinary.com/dz2arrelg/image/upload/v1691328239/bali-angklung_lkq3nr.jpg',
-                'price' => 75000,
-                'categories_id' => 1,
-                'partners_id' => 1,
-            ),
-            4 =>
-            array (
-                'name' => 'Pengenalan Kecapi Minangkabau',
-                'slug' => 'pengenalan-kecapi-minangkabau',
-                'description' => 'Pengenalan alat musik kecapi yang berasal dari Minangkabau, Sumatera Barat.',
-                'status' => 'ACTIVE',
-                'image' => 'https://res.cloudinary.com/dz2arrelg/image/upload/v1691328239/Kacapi-minangkabau_lahj0g.jpg',
-                'price' => 50000,
-                'categories_id' => 2,
-                'partners_id' => 1,
-            ),
-            5 =>
-            array (
                 'name' => 'Belajar Alat Musik Rinding',
                 'slug' => 'belajar-alat-musik-rinding',
                 'description' => 'Belajar memainkan alat musik rinding, alat musik khas daerah Jawa Barat.',
                 'status' => 'ACTIVE',
                 'image' => 'https://res.cloudinary.com/dz2arrelg/image/upload/v1695472522/Rinding_course_i2dwbx.jpg',
-                'price' => 100.000,
+                'price' => 35000,
                 'categories_id' => 3,
                 'partners_id' => 3,
             ),
-            6 =>
+            1 =>
             array (
                 'name' => 'Belajar Alat Musik Demung',
                 'slug' => 'belajar-alat-musik-demung',
@@ -97,7 +41,7 @@ class CoursesTableSeeder extends Seeder
                 'categories_id' => 2,
                 'partners_id' => 2,
             ),
-            7 =>
+            2 =>
             array (
                 'name' => 'Belajar Alat Musik Kendang',
                 'slug' => 'belajar-alat-musik-kendang',
